a8d0aa01647f274a6acbd44a50961510e46fc245
alexiabelendileva <84689539+alexiabelendileva@users.noreply.github.com>
2023-12-13 16:44:30 -0300

    updated nav

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-let counter = 1;
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-setInterval(() => {
-  document.getElementById('radio' + counter).checked = true;
-  counter++;
-  if (counter > 4) {
-    counter = 1;
-  }
-}, 4000);
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
